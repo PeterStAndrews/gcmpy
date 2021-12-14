6c21dccb9738229a84dbb1a407022c3f917cc4ef
Peter Mann <pm78@st-andrews.ac.uk>
2021-12-14 22:44:53 +0000

    Update utils.py

--- gcmpy/utils.py
+++ gcmpy/utils.py
@@ -1,4 +1,21 @@
-
+# Utilities for gcmpy
+#
+# Copyright (C) 2021 Peter Mann
+#
+# This file is part of gcmpy, generalised configuration model networks in Python.
+#
+# gcmpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# gcmpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gcmpy. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 from typing import Callable
 import numpy as np
