67647116075b034361129f97d603b0e9951cfb5c
PeterStAndrews <pm78@st-andrews.ac.uk>
2022-08-12 10:27:54 +0100

    v b

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@
 from setuptools import setup, find_packages
 
 # the current version of the package
-__version__ = "0.2.17"
+__version__ = "0.2.18"
 
 with open('README.rst') as f:
     longDescription = f.read()
