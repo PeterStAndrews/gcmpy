6b79ebd06515b5c8c53ebab6041349831112c8c0
PeterStAndrews <pm78@st-andrews.ac.uk>
2023-04-27 21:09:11 +0100

    Update tests

--- test/message_passing/test_equations.py
+++ test/message_passing/test_equations.py
@@ -5,36 +5,33 @@ from gcmpy.message_passing.equations.clique_equation import clique_equation
 from gcmpy.message_passing.equations.automated_equation import automated_equation
 
 
-class AutomatedEquationTest(unittest.TestCase):
+class EquationTestMixin():
+
     u: float = 0.651284213
     phi: float = 0.5645231765
 
-    def _initialise_us(self, G: nx.Graph, u: float = 0.5):
+    def _initialise_us(self, G: nx.Graph, u: float):
         us = {}
         for n in G.nodes():
             us[n] = u
         nx.set_node_attributes(G, us, "u")
 
-    def _make_edge(self, u: float = 0.5):
-        G = nx.Graph()
-        G.add_edges_from([(0, 1)])
-        self._initialise_us(G, u=u)
+    def _make_clique(self, n, u: float):
+        G = nx.complete_graph(n)
+        self._initialise_us(G, self.u)
         return G
 
-    def _make_triangle(self, u: float = 0.5):
-        G = nx.Graph()
-        G.add_edges_from([(0, 1), (1, 2), (2, 0)])
-        self._initialise_us(G, u=u)
-        return G
 
-    def _make_diamond(self, u: float = 0.5):
+class AutomatedEquationTest(EquationTestMixin, unittest.TestCase):
+
+    def _make_diamond(self, u: float):
         G = nx.Graph()
         G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 0), (0, 2)])
         self._initialise_us(G, u=u)
         return G
 
     def test_2_clique(self):
-        G = self._make_edge(u=self.u)
+        G = self._make_clique(n=2, u=self.u)
         self.assertAlmostEqual(
             automated_equation(G, self.phi, 0),
             1 - self.phi + self.u * self.phi,
@@ -42,7 +39,7 @@ class AutomatedEquationTest(unittest.TestCase):
         )
 
     def test_3_clique(self):
-        G = self._make_triangle(u=self.u)
+        G = self._make_clique(n=3, u=self.u)
         self.assertAlmostEqual(
             automated_equation(G, self.phi, 0),
             pow(1 - self.phi, 2)
@@ -87,17 +84,114 @@ class AutomatedEquationTest(unittest.TestCase):
             places=7,
         )
 
+    def test_4_clique(self):
+        T = self.phi
+        u = self.u
+        G = self._make_clique(n=4, u=u)
+        self.assertAlmostEqual(
+            automated_equation(G, self.phi, 1),
+            (
+                pow(1 - T, 3)
+                + 3 * u * T * pow(1 - T, 4)
+                + 3
+                * pow(u, 2)
+                * (pow(T, 3) * pow(1 - T, 3) + 3 * pow(T, 2) * pow(1 - T, 4))
+                + pow(u, 3)
+                * (
+                    pow(T, 6)
+                    + 6 * pow(T, 5) * (1 - T)
+                    + 15 * pow(T, 4) * pow(1 - T, 2)
+                    + 16 * pow(T, 3) * pow(1 - T, 3)
+                )
+            ),
+            places=7,
+        )
 
-class CliqueEquationTest(unittest.TestCase):
+    @unittest.skip("TODO - fix automated_equation() for 5 vertex motifs or more")
+    def test_5_clique(self):
+        T = self.phi
+        u = self.u
+        G = self._make_clique(n=5, u=u)
 
-    u: float = 0.651284213
-    phi: float = 0.5645231765
+        self.assertAlmostEqual(
+            automated_equation(G, T, 0),
+            (
+                pow(1 - T, 4)
+                + 4 * u * T * pow(1 - T, 6)
+                + 6
+                * pow(u, 2)
+                * (pow(T, 3) * pow(1 - T, 6) + 3 * pow(T, 2) * pow(1 - T, 7))
+                + 4
+                * pow(u, 3)
+                * (
+                    pow(T, 6) * pow(1 - T, 4)
+                    + 6 * pow(T, 5) * pow(1 - T, 5)
+                    + 15 * pow(T, 4) * pow(1 - T, 6)
+                    + 16 * pow(T, 3) * pow(1 - T, 7)
+                )
+                + pow(u, 4)
+                * (
+                    pow(T, 10)
+                    + 10 * pow(T, 9) * (1 - T)
+                    + 45 * pow(T, 8) * pow(1 - T, 2)
+                    + 120 * pow(T, 7) * pow(1 - T, 3)
+                    + 205 * pow(T, 6) * pow(1 - T, 4)
+                    + 222 * pow(T, 5) * pow(1 - T, 5)
+                    + 125 * pow(T, 4) * pow(1 - T, 6)
+                )
+            ),
+            places=7,
+        )
 
-    def _initialise_us(self, G: nx.Graph, u: float = 0.5):
-        us = {}
-        for n in G.nodes():
-            us[n] = u
-        nx.set_node_attributes(G, us, "u")
+    @unittest.skip("TODO - fix automated_equation() for 5 vertex motifs or more")
+    def test_6_clique(self):
+        T = self.phi
+        u = self.u
+        G = self._make_clique(n=6, u=u)
+        self.assertAlmostEqual(
+            automated_equation(G, self.phi, 0),
+            pow(1 - T, 5)
+            + 5 * u * T * pow(1 - T, 8)
+            + 10
+            * pow(u, 2)
+            * (pow(T, 3) * pow(1 - T, 9) + 3 * pow(T, 2) * pow(1 - T, 10))
+            + 10
+            * pow(u, 3)
+            * (
+                pow(T, 6) * pow(1 - T, 8)
+                + 6 * pow(T, 5) * pow(1 - T, 9)
+                + 15 * pow(T, 4) * pow(1 - T, 10)
+                + 16 * pow(T, 3) * pow(1 - T, 11)
+            )
+            + 5
+            * pow(u, 4)
+            * (
+                pow(T, 10) * pow(1 - T, 5)
+                + 10 * pow(T, 9) * pow(1 - T, 6)
+                + 45 * pow(T, 8) * pow(1 - T, 7)
+                + 120 * pow(T, 7) * pow(1 - T, 8)
+                + 205 * pow(T, 6) * pow(1 - T, 9)
+                + 222 * pow(T, 5) * pow(1 - T, 10)
+                + 125 * pow(T, 4) * pow(1 - T, 11)
+            )
+            + pow(u, 5)
+            * (
+                pow(T, 15) + 15 * pow(T, 14) * (1 - T)
+                +105 * pow(T, 13) * pow(1 - T, 2)
+                + 455 * pow(T, 12) * pow(1 - T, 3)
+                + 1365 * pow(T, 11) * pow(1 - T, 4)
+                + 2997 * pow(T, 10) * pow(1 - T, 5)
+                + 4945 * pow(T, 9) * pow(1 - T, 6)
+                + 6165 * pow(T, 8) * pow(1 - T, 7)
+                + 5700 * pow(T, 7) * pow(1 - T, 8)
+                + 3660 * pow(T, 6) * pow(1 - T, 9)
+                + 1296 * pow(T, 5) * pow(1 - T, 10)
+            ),
+            places=7,
+        )
+
+
+class CliqueEquationTest(EquationTestMixin, unittest.TestCase):
 
     def test_2_clique(self):
         self.assertAlmostEqual(
@@ -108,7 +202,7 @@ class CliqueEquationTest(unittest.TestCase):
 
     def test_3_clique(self):
         self.assertAlmostEqual(
-            clique_equation(3, self.phi, [self.u,self.u]),
+            clique_equation(3, self.phi, [self.u, self.u]),
             pow(1 - self.phi, 2)
             + 2 * self.phi * pow(1 - self.phi, 2) * self.u
             + (3 * pow(self.phi, 2) * (1 - self.phi) + pow(self.phi, 3))
@@ -116,13 +210,102 @@ class CliqueEquationTest(unittest.TestCase):
             places=7,
         )
 
-    def test_first_n_cliques(self):
-        m = 5
-        for n in range(2, m):
-            G = nx.complete_graph(n)
-            self._initialise_us(G, self.u)
-            self.assertAlmostEqual(
-                clique_equation(n, self.phi, [self.u]*(n-1)),
-                automated_equation(G, self.phi, 0),
-                places=7,
+    def test_4_clique(self):
+        T = self.phi
+        u = self.u
+        self.assertAlmostEqual(
+            (
+                pow(1 - T, 3)
+                + 3 * u * T * pow(1 - T, 4)
+                + 3
+                * pow(u, 2)
+                * (pow(T, 3) * pow(1 - T, 3) + 3 * pow(T, 2) * pow(1 - T, 4))
+                + pow(u, 3)
+                * (
+                    pow(T, 6)
+                    + 6 * pow(T, 5) * (1 - T)
+                    + 15 * pow(T, 4) * pow(1 - T, 2)
+                    + 16 * pow(T, 3) * pow(1 - T, 3)
+                )
+            ),
+            clique_equation(4, self.phi, [self.u, self.u, self.u]),
+            places=7,
+        )
+
+    def test_5_clique(self):
+        T = self.phi
+        u = self.u
+        self.assertAlmostEqual(
+            (
+                pow(1 - T, 4)
+                + 4 * u * T * pow(1 - T, 6)
+                + 6
+                * pow(u, 2)
+                * (pow(T, 3) * pow(1 - T, 6) + 3 * pow(T, 2) * pow(1 - T, 7))
+                + 4
+                * pow(u, 3)
+                * (
+                    pow(T, 6) * pow(1 - T, 4)
+                    + 6 * pow(T, 5) * pow(1 - T, 5)
+                    + 15 * pow(T, 4) * pow(1 - T, 6)
+                    + 16 * pow(T, 3) * pow(1 - T, 7)
+                )
+                + pow(u, 4)
+                * (
+                    pow(T, 10)
+                    + 10 * pow(T, 9) * (1 - T)
+                    + 45 * pow(T, 8) * pow(1 - T, 2)
+                    + 120 * pow(T, 7) * pow(1 - T, 3)
+                    + 205 * pow(T, 6) * pow(1 - T, 4)
+                    + 222 * pow(T, 5) * pow(1 - T, 5)
+                    + 125 * pow(T, 4) * pow(1 - T, 6)
+                )
+            ),
+            clique_equation(5, self.phi, [self.u] * (5 - 1)),
+            places=7,
+        )
+
+    def test_6_clique(self):
+        T = self.phi
+        u = self.u
+        self.assertAlmostEqual(
+            pow(1 - T, 5)
+            + 5 * u * T * pow(1 - T, 8)
+            + 10
+            * pow(u, 2)
+            * (pow(T, 3) * pow(1 - T, 9) + 3 * pow(T, 2) * pow(1 - T, 10))
+            + 10
+            * pow(u, 3)
+            * (
+                pow(T, 6) * pow(1 - T, 8)
+                + 6 * pow(T, 5) * pow(1 - T, 9)
+                + 15 * pow(T, 4) * pow(1 - T, 10)
+                + 16 * pow(T, 3) * pow(1 - T, 11)
             )
+            + 5
+            * pow(u, 4)
+            * (
+                pow(T, 10) * pow(1 - T, 5)
+                + 10 * pow(T, 9) * pow(1 - T, 6)
+                + 45 * pow(T, 8) * pow(1 - T, 7)
+                + 120 * pow(T, 7) * pow(1 - T, 8)
+                + 205 * pow(T, 6) * pow(1 - T, 9)
+                + 222 * pow(T, 5) * pow(1 - T, 10)
+                + 125 * pow(T, 4) * pow(1 - T, 11)
+            )
+            + pow(u, 5)
+            * (
+                pow(T, 15) + 15 * pow(T, 14) * (1 - T)
+                +105 * pow(T, 13) * pow(1 - T, 2)
+                + 455 * pow(T, 12) * pow(1 - T, 3)
+                + 1365 * pow(T, 11) * pow(1 - T, 4)
+                + 2997 * pow(T, 10) * pow(1 - T, 5)
+                + 4945 * pow(T, 9) * pow(1 - T, 6)
+                + 6165 * pow(T, 8) * pow(1 - T, 7)
+                + 5700 * pow(T, 7) * pow(1 - T, 8)
+                + 3660 * pow(T, 6) * pow(1 - T, 9)
+                + 1296 * pow(T, 5) * pow(1 - T, 10)
+            ),
+            clique_equation(6, T, [u] * (6 - 1)),
+            places=7,
+        )
