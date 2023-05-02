a7e01b48abf570fb0a9bb290dd1f8d31a3d565ba
PeterStAndrews <pm78@st-andrews.ac.uk>
2023-05-02 10:08:34 +0100

    speed improvements of automated equation

--- gcmpy/message_passing/equations/automated_equation.py
+++ gcmpy/message_passing/equations/automated_equation.py
@@ -8,9 +8,14 @@ class AutomatedEquation():
         self._edge_combinations = {}
         self._connected_subgraphs = {}
 
-    def get_connected_subgraphs(self, G: nx.Graph, root: int):
+    def get_connected_subgraphs(self, G: nx.Graph, root: int) -> list[set]:
         """
         Cache for connected subgraphs of G containing root.
+        Args:
+            G nx.Graph: the subgraph whose components we want
+            root int: the root vertex in G
+        Returns:
+            list[set]: subgraph components.
         """
         key: str = f"{root}-{G.name}"
         if key in self._connected_subgraphs:
@@ -39,12 +44,11 @@ class AutomatedEquation():
             new_possible: set = (possible | set(G.neighbors(j))) - excluded
             self._get_connected_subgraphs(G, new_subgraph, new_possible, excluded, results, max_size)
 
-
-    def get_edge_combinations(self, G: nx.Graph, c: list[int]) -> list[tuple]:
+    def get_edge_combinations(self, G: nx.Graph, c: list[int]) -> list[int]:
         """
-        Returns the edge combinations of a subgraph can be removed and still
-        yield a connected subgraph. This routine is expensive, so we cache
-        the combinations under the motif ID.
+        Returns the number of edges in a valid edge combinations of a subgraph can be removed and still
+        yield a connected subgraph. This routine is expensive, so we cache the number of edges in each
+        combination under the motif ID.
         """
         key: str = f"{c}-{G.name}"
         if key in self._edge_combinations:
@@ -59,11 +63,22 @@ class AutomatedEquation():
                 g_test: nx.Graph = G.copy()
                 g_test.remove_edges_from(es)
                 if nx.is_connected(g_test):
-                    edge_combinations_final.append(es)
+                    edge_combinations_final.append(len(es))
 
             self._edge_combinations[key] = edge_combinations_final
             return self._edge_combinations[key]
 
+    def get_us(self, G: nx.Graph, root: int) -> float:
+        """
+        Returns product of u values for each vertex in ns. Assumes that
+        G has a attribute keyed by 'u', whose expected value is a float.
+        """
+        product = 1.0
+        for n in G.nodes():
+            if n == root:
+                continue
+            product *= G.nodes[n]["u"]
+        return product
 
     def automated_equation(self, G: nx.Graph, p: float, root: int) -> float:
         """
@@ -77,19 +92,6 @@ class AutomatedEquation():
         Returns:
             `prob` (float): probability `root` not in giant component via `G`
         """
-
-        def get_us(G: nx.Graph, root: int) -> float:
-            """
-            Returns product of u values for each vertex in ns. Assumes that
-            G has a attribute keyed by 'u', whose expected value is a float.
-            """
-            product = 1.0
-            for n in G.nodes():
-                if n == root:
-                    continue
-                product *= G.nodes[n]["u"]
-            return product
-
         prob = 0.0
 
         # `components` is a list of combinations of vertices that contain `root`
@@ -109,33 +111,32 @@ class AutomatedEquation():
             g = G.copy()
 
             # remove interface and inconsequential edges
+            edges_to_remove = []
             for e in G.edges():
                 if (e[0] not in c) and (e[1] not in c):
                     # remove edges between vertex pairs that are not in the same component `root`,
                     # it does not contribute to the equation
-                    g.remove_edge(*e)
+                    edges_to_remove.append(e)
                 elif (e[0] in c) and (e[1] in c):
                     # edge been vertices in same component as `root`
                     continue
                 else:
                     # one end is in component, other is out - interface edge, must be (1-p)
-                    g.remove_edge(*e)
+                    edges_to_remove.append(e)
                     interface_edges *= (1 - p)
                     continue
+
+            g.remove_edges_from(edges_to_remove)
 
             # remove isolated vertices from RG
             g.remove_nodes_from([n for n in g.nodes() if len(list(g.neighbors(n))) == 0])
 
-
             # remaining edges in g are between vertices in the same component as `root`
             # they can be either p or 1-p state as long as the component is connected
-            edge_combinations = self.get_edge_combinations(g, c)
-            for es in edge_combinations:
-                g_test = g.copy()
-                g_test.remove_edges_from(es)
-                us = get_us(g_test, root)
+            us = self.get_us(g, root)
+            for n_edges in self.get_edge_combinations(g, c):
                 prob += (
-                    (pow(p, len(g_test.edges())) * pow(1 - p, len(es)))
+                    (pow(p, len(g.edges()) - n_edges) * pow(1 - p, n_edges))
                     * interface_edges
                     * us
                 )
